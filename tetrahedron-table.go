273ce36ad071d8ae079cde2de70a6cca5eda7975
Megidd Git <megiddgit@gmail.com>
2023-05-13 19:01:16 +0330

    Case 1 result

--- tetrahedron-table.go
+++ tetrahedron-table.go
@@ -8,14 +8,25 @@ package main
 // Inspired by:
 // https://github.com/deadsy/sdfx/blob/1a71e404e4b2aa00c59f53cffc219a9e83e62d85/render/march3.go#L360
 var mcTetrahedronTable = [256][]int{
-	{}, // 0b00000000 case 0: no cube corner has zero/negative value.
-	{}, // 0b00000001 case 1: first cube corner has zero/negative value.
-	{}, // 0b00000010
-	{}, // 0b00000011
-	{}, // 0b00000100
+	// 0b00000000 case 0: no cube corner has zero/negative value.
+	{},
+	// 0b00000001 case 1: first cube corner has zero/negative value.
+	{12, 0, 3, 8},
+	// 0b00000010
+	{},
+	// 0b00000011
+	{},
+	// 0b00000100
+	{},
+
 	// ...
-	{}, // 0b11111100
-	{}, // 0b11111101
-	{}, // 0b11111110
-	{}, // 0b11111111 case 255: all cube corners have zero/negative values.
+
+	// 0b11111100
+	{},
+	// 0b11111101
+	{},
+	// 0b11111110
+	{},
+	// 0b11111111 case 255: all cube corners have zero/negative values.
+	{},
 }
