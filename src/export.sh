82fa8fcce6c26721612bc58a92f47f279bf6f46f
Megidd Git <megiddgit@gmail.com>
2023-06-29 12:40:33 +0330

    Calibrate camera

--- src/export.sh
+++ src/export.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-openscad --o output.png --export-format png --camera 100,-100,100,40,40,0 --autocenter --viewall case.scad
+openscad --o output.png --export-format png --camera 100,-180,100,40,40,40 --autocenter --viewall case.scad
