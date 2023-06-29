519104eac743690400ad54fbf862a46237332d85
Megidd Git <megiddgit@gmail.com>
2023-06-29 13:52:09 +0330

    Export 15 PNG files

--- src/export.sh
+++ src/export.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-openscad --o output.png --export-format png --camera 100,-180,100,40,40,40 --autocenter --viewall case.scad
+for i in {0..14}
+do
+  output_file="output$i.png"
+  openscad --o $output_file --export-format png --camera 100,-180,100,40,40,40 --autocenter --viewall --D i=$i case.scad
+done
