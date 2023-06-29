57cc26893bcd29e749ca72680426e926b0c32c72
Megidd Git <megiddgit@gmail.com>
2023-06-29 17:10:16 +0330

    Export last 15 cases as PNG

--- src/export.sh
+++ src/export.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for i in {0..14}
+#for i in {0..14}
+for i in {241..255}
 do
   output_file="output$i.png"
   openscad --o $output_file --export-format png --camera 100,-180,100,40,40,40 --autocenter --viewall --D i=$i case.scad
